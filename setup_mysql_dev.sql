de9f0f4a0077412584d34dff9836c4e0956096bc
Ikennaf1 <iiikbros202@gmail.com>
2023-03-19 15:40:26 +0100

    Update FileStorage

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
--- Set ALL privileges on the database `hbnb_test_db` to the `hbnb_test` user.
--- Set SELECT privileges on the database `performance_schema` to the `hbnb_test` user.
+-- Set ALL privileges on the database `hbnb_dev_db` to the `hbnb_dev` user.
+-- Set SELECT privileges on the database `performance_schema` to the `hbnb_dev` user.
 
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-DROP USER IF EXISTS 'hbnb_test'@'localhost';
-CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
